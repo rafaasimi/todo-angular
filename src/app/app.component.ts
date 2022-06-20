c665fc2b6482e33662abaa50bf0961f3e75a7fd3
Rafael Simionato Zorzenon <rafael_zorzenon@sicredi.com.br>
2022-06-20 13:27:08 -0300

    Creating methods to remove, complete and remake tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,27 @@ import { Todo } from 'src/models/todo.model';
 })
 export class AppComponent {
   public todos: Todo[] = [];
-  public title: string = 'Minhas tarefas'
+  public title: string = 'Minhas tarefas';
 
   constructor() {
-    this.todos.push(new Todo(1,'Simular financiamento', false));
-    this.todos.push(new Todo(2,'Assinar contrato', false));
-    this.todos.push(new Todo(3,'Cortar cabelo', true));
+    this.todos.push(new Todo(1, 'Simular financiamento', false));
+    this.todos.push(new Todo(2, 'Assinar contrato', false));
+    this.todos.push(new Todo(3, 'Cortar cabelo', true));
   }
 
+  remove(todo: Todo): void {
+    const index = this.todos.indexOf(todo);
+
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone(todo: Todo): void {
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo): void {
+    todo.done = false;
+  }
 }
