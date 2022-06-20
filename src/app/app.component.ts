9d06ba7cd26b2a6ebc8b4248353e249e858900bd
Rafael Simionato Zorzenon <rafael_zorzenon@sicredi.com.br>
2022-06-20 13:13:28 -0300

    Creation of the initial structure and modeling of tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: string = 'Minhas tarefas'
+
+  constructor() {
+    this.todos.push(new Todo(1,'Simular financiamento', false));
+    this.todos.push(new Todo(2,'Assinar contrato', false));
+    this.todos.push(new Todo(3,'Cortar cabelo', true));
+  }
+
 }
