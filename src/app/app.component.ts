c9cfa5d95422ee1fc8e98dd5b07706e40b44fa44
Rafael Simionato Zorzenon <rafael_zorzenon@sicredi.com.br>
2022-06-20 13:49:47 -0300

    Imported ReactiveForm and created formGroup first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -9,11 +10,20 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
   public todos: Todo[] = [];
   public title: string = 'Minhas tarefas';
+  public form: FormGroup;
 
-  constructor() {
+    constructor(private fb: FormBuilder) {
     this.todos.push(new Todo(1, 'Simular financiamento', false));
     this.todos.push(new Todo(2, 'Assinar contrato', false));
     this.todos.push(new Todo(3, 'Cortar cabelo', true));
+
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required,
+      ])]
+    })
   }
 
   remove(todo: Todo): void {
